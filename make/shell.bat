ad4e60d1cc9f7c53f43d8b96dbcd684e0799dcad
Robert Pelnar <robert.pelnar@seznam.cz>
2020-01-12 20:55:52 +0100

    scripts to start shell with env. variables and path (windows,mingw)

---
+++ make/shell.bat
@@ -0,0 +1,10 @@
+CD ..
+SET PATH=C:\Program Files (x86)\Atollic\TrueSTUDIO for STM32 9.3.0\Tools;%PATH%
+SET PATH=C:\Program Files (x86)\Atollic\TrueSTUDIO for STM32 9.3.0\PCTools\bin;%PATH%
+SET PATH=C:\Program Files (x86)\Atollic\TrueSTUDIO for STM32 9.3.0\ARMTools\bin;%PATH%
+
+SET DIRSEP=\\
+SET DIR_RM=RMDIR /S /Q
+SET DIR_MK=MKDIR
+
+cmd
---
+++ make/shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+
+cd ..
+export PATH="\
+/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/Tools:\
+/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/PCTools/bin:\
+/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/ARMTools/bin:\
+"$PATH
+
+bash -i
