4edea440d86ee625b108cb3ab0179c4e58868d68
Robert Pelnar <robert.pelnar@seznam.cz>
2020-01-23 21:06:43 +0100

    Removed check from cmake

--- CMake.mk
+++ CMake.mk
@@ -7,10 +7,6 @@ CMAKE_BUILD_TYPE := $(word 2,$(CMAKE_CONFIG))
 CMAKE_BOOTLOADER := $(subst boot,,$(word 3,$(CMAKE_CONFIG)))
 
 cmake:
-# check valid build configuration for cmake
-ifneq (,$(filter-out Debug_Boot Release_Boot mini_debug_emptyboot mini_release_emptyboot, $(BUILD_CONFIGURATION)))
-$(error invalid configuration "$(BUILD_CONFIGURATION)")
-endif
 	@echo CMAKE_CONFIG=$(CMAKE_CONFIG)
 	@echo CMAKE_PRINTER=$(CMAKE_PRINTER)
 	@echo CMAKE_BUILD_TYPE=$(CMAKE_BUILD_TYPE)
