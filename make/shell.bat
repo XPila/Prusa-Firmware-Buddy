814269d3b86132a53a0ed88b2fd450eba2be24f1
Robert Pelnar <robert.pelnar@seznam.cz>
2020-01-23 17:19:38 +0100

    commandline shell scripts updated

--- make/shell.bat
+++ make/shell.bat
@@ -3,8 +3,9 @@ SET PATH=C:\Program Files (x86)\Atollic\TrueSTUDIO for STM32 9.3.0\Tools;%PATH%
 SET PATH=C:\Program Files (x86)\Atollic\TrueSTUDIO for STM32 9.3.0\PCTools\bin;%PATH%
 SET PATH=C:\Program Files (x86)\Atollic\TrueSTUDIO for STM32 9.3.0\ARMTools\bin;%PATH%
 
-SET DIRSEP=\\
-SET DIR_RM=RMDIR /S /Q
-SET DIR_MK=MKDIR
+SET TOOLCHAIN_PREFIX=arm-atollic-eabi
+SET PATH_SEPARATOR=\\
+SET SHELL_RMDIR=RMDIR /S /Q
+SET SHELL_MKDIR=MKDIR
 
 cmd
--- make/shell.sh
+++ make/shell.sh
@@ -2,10 +2,9 @@
 #
 
 cd ..
-export PATH="\
-/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/Tools:\
-/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/PCTools/bin:\
-/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/ARMTools/bin:\
-"$PATH
+export PATH="/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/Tools:"$PATH
+export PATH="/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/PCTools/bin:"$PATH
+export PATH="/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/ARMTools/bin:"$PATH
+export TOOLCHAIN_PREFIX=arm-atollic-eabi
 
 bash -i
