96ffc733c1194ab9de8b75f0da26800fdcb9f63d
Robert Pelnar <robert.pelnar@seznam.cz>
2020-01-25 22:39:14 +0100

    Make - multiline

---
+++ make/Multiline.mk
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# Constants for generating multiline output (debug, project generators)
+
+# space
+SPACE :=
+SPACE +=
+
+# newline
+define NEWLINE
+
+
+endef
