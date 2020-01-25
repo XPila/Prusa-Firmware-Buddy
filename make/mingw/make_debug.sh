25c7e61e45f3484f0ee2b85b0f970491d221443d
Robert Pelnar <robert.pelnar@seznam.cz>
2020-01-25 22:39:56 +0100

    mingw build script

---
+++ make/mingw/make_debug.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+
+cd ..
+export PATH="/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/Tools:"$PATH
+export PATH="/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/PCTools/bin:"$PATH
+export PATH="/c/Program Files (x86)/Atollic/TrueSTUDIO for STM32 9.3.0/ARMTools/bin:"$PATH
+export TOOLCHAIN_PREFIX=arm-atollic-eabi
+
+bash -i
